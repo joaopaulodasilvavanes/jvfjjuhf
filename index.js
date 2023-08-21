d6cf90e02b00d45e7afda8dc25a481eab3c6d083
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-08-21 17:31:11 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+* primeiro programa em javascript
+* joão paulo
+*/
+console.log("ola mundo!");
+prompt("qual é o seu nome?");
+const VariavelA = 100;
+console.log (VariavelA)
+const VariavelB=VariavelA+ 50;
+console.log(VariavelA);
+console.log(VariavelB);
+//console.log (VariavelC);
+let numero = 5;
+console.log(numero);
+    numero = 120;
+    console.log(numero) ;
+//concatenar textos
+/*
+let nome = "João paulo";
+
+let sobrenome = "Vanes";
+let idade = 16;
+console.log("sOlá" nome,sobreNome, "voce tem", idade ,"anos");
+*/
+const variavelfalsa = false;
+
+console.log("Variavel  falsa")
+//exercicio 1
+let nome = "João paulo";
+let sobrenome = "Vanes"
+let idade = 16;
+let status = "você é estudante";
+console.log("Ola",nome,sobrenome,idade,status);
+//typeof
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof estudante);
+const vazio = null;
+console.log (typeof vazio);
+
+//PROMPT
+let valor =prompt("seu nome");
+console.log(valor);
+
+// exercicio 2
+let exercicio = prompt("digite seu nome");
+let atividade = prompt("digite sua idade");
+console.log(typeof exercicio);
+console.log (typeof atividade);
+
+
+
